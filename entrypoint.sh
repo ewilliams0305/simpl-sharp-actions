9f020d30a9d88e247bcc4424098d6723ffa84636
Eric Williams <ewilliams0305@gmail.com>
2024-07-19 11:59:53 -0400

    created variables for all file paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,9 @@ project_name=$(basename "$1" .csproj)
 assembly_name=$(basename "$1" .dll)
 csproj_file_path="/github/workspace/$1"
 clz_file_name="${project_name}.clz"
+dll_file_name="${project_name}.dll"
 clz_file_path="/github/workspace/bin/$2/$clz_file_name"
+dll_file_path="/github/workspace/bin/$2/$dll_file_name"
 
 dotnet dotnet tool install --local SimplSharp.Tool --version 0.2.0 --create-manifest-if-needed 
 
@@ -18,7 +20,10 @@ dotnet simplsharp targets -d /github/workspace/$directory
 
 dotnet build $csproj_file_path -c $3 -o /github/workspace/bin/$2
 
-dotnet simplsharp clz -p "/github/workspace/$1" -a /github/workspace/bin/$2/$assembly_name 
+echo "Target Project: $csproj_file_path"
+echo "Target Assembly: $dll_file_path"
+
+dotnet simplsharp clz -p $csproj_file_path -a $dll_file_path
 
 if [ ! -f "$clz_file_path" ]; then
   echo "Error: CLZ file not found at $clz_file_path"
