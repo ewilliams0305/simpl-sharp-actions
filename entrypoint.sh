9efc65b789955ecbaff05e14db86a6977b597bae
Eric Williams <ewilliams0305@gmail.com>
2024-07-19 12:16:46 -0400

    added artifact output

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ assembly_name=$(basename "$1" .dll)
 csproj_file_path="/github/workspace/$1"
 clz_file_name="${project_name}.clz"
 dll_file_name="${project_name}.dll"
-clz_file_path="/github/workspace/bin/$2/$clz_file_name"
+clz_file_path="/github/workspace/clz/$2/$clz_file_name"
 dll_file_path="/github/workspace/bin/$2/$dll_file_name"
 
 dotnet dotnet tool install --local SimplSharp.Tool --version 0.2.0 --create-manifest-if-needed 
@@ -23,7 +23,7 @@ dotnet build $csproj_file_path -c $3 -o /github/workspace/bin/$2
 echo "Target Project: $csproj_file_path"
 echo "Target Assembly: $dll_file_path"
 
-dotnet simplsharp clz -p $csproj_file_path -a $dll_file_path
+dotnet simplsharp clz -p $csproj_file_path -a $dll_file_path -d /github/workspace/clz/$2
 
 if [ ! -f "$clz_file_path" ]; then
   echo "Error: CLZ file not found at $clz_file_path"
@@ -33,3 +33,4 @@ fi
 echo "Created CLZ $clz_file_path"
 
 echo "clz_file=$clz_file_path" >> $GITHUB_OUTPUT
+echo "artifacts_path='/github/workspace/clz/' >> $GITHUB_OUTPUT
