e0f7d79bf77ba6e5231808f258adfbfff21ad752
Eric Williams <ewilliams0305@gmail.com>
2024-07-19 11:34:58 -0400

    added hard exits

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,25 @@
 
 echo "Running simpl-sharp-action against project $1 $2 $3"
 
+# EXIT IF COMMANDS FAIL
+set -e
+
 directory=$(dirname "$1")
 project_name=$(basename "$1" .csproj)
-clz_file="${project_name}.clz"
+clz_file_name="${project_name}.clz"
+clz_file_path="/github/workspace/bin/$2/$clz_file_name"
 
 dotnet dotnet tool install --local SimplSharp.Tool --version 0.2.0 --create-manifest-if-needed 
 
 dotnet simplsharp targets -d /github/workspace/$directory
 
-dotnet build /github/workspace/$1 -c $3 -o ./bin/$2
+dotnet build /github/workspace/$1 -c $3 -o /github/workspace/bin/$2
 
-echo "clz_file=./bin/$2/$clz_file" >> $GITHUB_OUTPUT
+if [ ! -f "$clz_file_path" ]; then
+  echo "Error: CLZ file not found at $clz_file_path"
+  exit 1
+fi
+
+echo "Created CLZ $clz_file_path"
+
+echo "clz_file=./bin/$2/$clz_file_name" >> $GITHUB_OUTPUT
